7c220101850c2bb2b83e81909239ef156e26bf32
Maskitkk <maskitkk@gmail.com>
2024-03-27 14:19:50 +0300

    Added project

---
+++ Таймер/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Таймер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button30mins = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.Button1.Location = new System.Drawing.Point(75, 99);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(416, 41);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Выключить через час";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(75, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(416, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выключить через 2 часа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(75, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(416, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Выключить через полтора часа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.progressBar1.Location = new System.Drawing.Point(75, 301);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(416, 58);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выключение через: ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Отмена";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button30mins
+            // 
+            this.button30mins.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold);
+            this.button30mins.Location = new System.Drawing.Point(75, 33);
+            this.button30mins.Name = "button30mins";
+            this.button30mins.Size = new System.Drawing.Size(416, 44);
+            this.button30mins.TabIndex = 6;
+            this.button30mins.Text = "Выключить через полчаса";
+            this.button30mins.UseVisualStyleBackColor = true;
+            this.button30mins.Click += new System.EventHandler(this.button30mins_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(560, 402);
+            this.Controls.Add(this.button30mins);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Мой собственный самый лучший в мире таймер выключения компа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button30mins;
+    }
+}
+
---
+++ Таймер/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+
+namespace Таймер
+
+{
+    public partial class Form1 : Form
+    {
+
+        
+        private int remainingSeconds = 0;
+        private int totalSeconds = 0;
+        private Thread countdownThread;
+        private NotifyIcon notifyIcon;
+        public Form1()
+
+        {
+            InitializeComponent();
+            InitializeNotifyIcon();
+            this.FormClosing += Form1_FormClosing;
+
+        }
+
+        private void InitializeNotifyIcon()
+        { 
+        notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = this.Icon;
+            notifyIcon.Visible = false;
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem showMenuItem = new ToolStripMenuItem("Открыть");
+            showMenuItem.Click += ShowMenuItem_Click;
+            contextMenuStrip.Items.Add(showMenuItem);
+
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Выход");
+            exitMenuItem.Click += ExitMenuItem_Click;
+            contextMenuStrip.Items.Add(exitMenuItem);
+
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+           if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            StopCountdown();    
+            Application.Exit();
+        }
+
+        private void ShowMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+      
+
+        private void StartCountdown(int durationSeconds)
+        {
+            {
+                remainingSeconds = durationSeconds;
+                totalSeconds = durationSeconds;
+                countdownThread = new Thread(new ThreadStart(CountdownThread));
+                countdownThread.Start();
+            }
+        }
+
+        private void CountdownThread()
+        { while (remainingSeconds > 0)
+            
+            { if (IsStopRequested())
+                {
+                    break; 
+                }
+
+
+            Updatelabel1();
+        
+            Thread.Sleep(1000);
+           
+            remainingSeconds--;
+            }
+        if (remainingSeconds <= 0)
+            { Process.Start("shutdown", "/s /t0"); 
+            }
+
+        }
+        
+        
+        
+        private void Updatelabel1()
+        { 
+        TimeSpan timeSpan = TimeSpan.FromSeconds(remainingSeconds);
+            string formattedTime = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new MethodInvoker(delegate { label1.Text = formattedTime; }));
+            }
+            else { label1.Text = formattedTime; }
+
+            UpdateProgressBar();
+        
+        
+        }
+
+        private void button30mins_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 1800");
+            timer1.Start();
+            StartCountdown(1800);
+        }
+        
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 3600");
+            timer1.Start();
+            StartCountdown(3600);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 7200");
+            timer1.Start();
+            StartCountdown(7200);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 5400");
+            timer1.Start();
+            StartCountdown(5400);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "-a");
+            StopCountdown();
+        }
+
+
+        private void StopCountdown()
+        {
+            if (countdownThread != null && countdownThread.IsAlive)
+            {
+                countdownThread.Abort();
+                remainingSeconds = 0;
+                Updatelabel1();
+
+            }
+
+        }
+
+        private bool IsStopRequested()
+        { if (InvokeRequired)
+            {
+                return (bool)Invoke(new Func<bool>(IsStopRequested));
+            }
+            return remainingSeconds <= 0;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+       {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(delegate {  UpdateProgressBar(); })); 
+            }
+            else
+            {
+                progressBar1.Maximum = totalSeconds;
+                progressBar1.Value = totalSeconds - remainingSeconds;
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
